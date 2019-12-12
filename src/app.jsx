fef4160e76c18e88242259bcf76c180632c84533
Lance Smith <lance.matthew.smith@gmail.com>
2019-12-11 17:43:03 -0800

    Cleanup

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./css/app.scss";
+
+const App = () => (
+  <>
+    <header>
+      <h1>Title</h1>
+    </header>
+
+    <section>
+      <h2>main content</h2>
+    </section>
+
+    <footer>
+      <p>Footer</p>
+    </footer>
+  </>
+);
+
+export default App;
